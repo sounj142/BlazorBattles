66c0d5201f307276c422ff42a89f8883edc0c76c
Hoang <sounj142@gmail.com>
2021-12-13 15:01:53 +0700

    Add TopMenu component

---
+++ BlazorBattles/Client/Shared/TopMenu.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorBattles.Client.Shared
+{
+    public partial class TopMenu : ComponentBase
+    {
+        public int Bananas { get; set; } = 100;
+    }
+}
