10b134e213cb3e928ed3cd61328b6d2c4c22c829
Hoang <sounj142@gmail.com>
2021-12-14 16:20:12 +0700

    clean up code

---
+++ BlazorBattles/Server/Controllers/UnitController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorBattles.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UnitController : ControllerBase
+    {
+        public UnitController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("");
+        }
+    }
+}
